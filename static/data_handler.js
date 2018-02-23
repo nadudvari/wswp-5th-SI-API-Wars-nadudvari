6785051875328254aa26743e65bda0ea16d2fe05
nadudvari <nadudvari@github.com>
2018-02-23 15:36:16 +0100

    Commit before deployment

--- static/data_handler.js
+++ static/data_handler.js
@@ -1,11 +1,4 @@
 
 let dataHandler = {
+    };
 
-    getResidentsApi: function () {
-        let residentsAPI = document.getElementsByClassName('residentsData');
-        for (let i = 0, i < residentsAPI.length, i++){
-            console.log(residentsAPI[i])
-        }
-    }
-};
-getResidentsApi();
